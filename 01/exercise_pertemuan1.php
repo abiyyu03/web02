ede63ac767e65b97ca379f89d44db41e1200f9b8
abiyyu03 <abiyyu652@gmail.com>
2024-02-19 10:31:59 +0700

    add pertemuan1 exercise

---
+++ 01/exercise_pertemuan1.php
@@ -0,0 +1,17 @@
+<?php
+
+/**
+ * Selamat datang di exercise PHP Pertemuan 1
+ * silakan kalian buat kode nya dibawah 'TODO'
+ */
+
+// TODO 1 : buatlah 2 variabel bebas, yang satu berisi kata-kata (string) yang satu lagi berisi nomor (integer)
+
+
+// TODO 2 : buatlah 1 konstanta bebas
+
+
+// TODO 3 : buatlah array berisi nama hewan berikut : Gajah, Singa, Kuda, Ular, Kucing, Jerapah
+
+
+// TODO 4 : tampilkan hewan "gajah" pada arrah hewan diatas, hint : akses nilai array dengan nomor index nya
